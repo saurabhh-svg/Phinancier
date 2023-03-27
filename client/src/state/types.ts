0db3856d5fb8e15ac13876b0e49b38e3201aed1a
saurabhh-svg <singhsaurabh2901@gmail.com>
2023-03-28 01:02:44 +0530

    feat: api integration and graph creation

---
+++ client/src/state/types.ts
@@ -0,0 +1,34 @@
+export interface ExpensesByCategory {
+  salaries: number;
+  supplies: number;
+  services: number;
+}
+
+export interface Month {
+  id: string;
+  month: string;
+  revenue: number;
+  expenses: number;
+  nonOperationalExpenses: number;
+  operationalExpenses: number;
+}
+
+export interface Day {
+  id: string;
+  day: string;
+  revenue: number;
+  expenses: number;
+}
+
+export interface GetKpisResponse {
+  id: string;
+  _id: string;
+  __v: number;
+  totalProfit: number;
+
+  totalRevenue: number;
+  totalExpenses: number;
+  expensesByCategory: ExpensesByCategory;
+  monthlyData: Array<Month>;
+  dailyData: Array<Day>;
+}
